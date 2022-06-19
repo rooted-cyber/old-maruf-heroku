9bc468540bc0f457bce45b88d3236340d83f203e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-19 12:26:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## Deploy to Heroku 
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ rootedcyber.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/rooted-cyber/old-maruf-heroku
+cd old-ma*
+unzip ult.zip
+printf "\n\n Ultroid starting \n\n"
+bash startup
